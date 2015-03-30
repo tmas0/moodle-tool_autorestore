c8d719c6c98c3e5c3a043eb9c1b4c0b5249af1f4
Dextirpe <ricardo.diaz@uib.es>
2015-03-30 09:34:58 +0200

    Create autorestore.php

---
+++ lang/en/autorestore.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'local_automatedrestore', language 'en'.
+ *
+ * @package   tool_autorestore
+ * @copyright  2015 Universitat de les Illes Balears http://www.uib.es
+ * @author     Toni Mas, Ricardo Díaz
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['aftersaving...'] = 'Once you have saved your settings, you may wish to';
+$string['pluginname'] = 'Automated restore';
+$string['autorestorecrontask'] = 'automatedrestore';
+$string['restore'] = 'Restore';
+$string['restoredcourses'] = 'Restored courses';
+$string['pluginname_desc'] = 'Description';
+$string['basicsettings'] = 'Description';
+$string['location'] = 'Files location';
+$string['logtolocation'] = 'Log file output location (blank for no logging)';
+$string['mailadmins'] = 'Notify admin by email';
