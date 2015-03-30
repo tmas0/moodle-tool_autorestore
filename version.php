26eba4317a7bc20262d884ffbbcae79b560419aa
Dextirpe <ricardo.diaz@uib.es>
2015-03-30 09:23:31 +0200

    Create version.php

---
+++ version.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ * @package   tool_autorestore
+ * @copyright  2015 Universitat de les Illes Balears http://www.uib.es
+ * @authors     Toni Mas, Ricardo Díaz
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2015033000;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2014051200;        // Requires this Moodle version.
+$plugin->cron = 0;
+$plugin->release = 'Version 1.0';
+$plugin->component = 'tool_autorestore'; // Full name of the plugin (used for diagnostics).
